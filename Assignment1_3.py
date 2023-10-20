a8e25365eba2d5192ee2dd3cd143253a9e3792d1
lsq-647 <144921823+lsq-647@users.noreply.github.com>
2023-10-21 00:04:21 +0800

    Add files via upload

---
+++ Assignment1_3.py
@@ -0,0 +1,21 @@
+# 3. Pascal triangle
+def Pascal_triangle(k):
+    line = [[1], [1, 1]]
+    for i in range(2, k):
+        pre = line[i-1]
+        cul = [1]
+        for j in range(i-1):
+            cul.append(pre[j] + pre[j+1])
+        cul.append(1)
+        line.append(cul)
+    return cul
+
+Pascal_triangle(100)
+Pascal_triangle(200)
+
+# I consulted this website to help me: https://blog.csdn.net/W_chuanqi/article/details/123679167
+
+
+
+
+
