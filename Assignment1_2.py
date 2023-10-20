0820f95c2d4562fef15508972a56e501e6f07e43
lsq-647 <144921823+lsq-647@users.noreply.github.com>
2023-10-20 16:16:18 +0800

    Add files via upload

---
+++ Assignment1_2.py
@@ -0,0 +1,25 @@
+# 2. Matrix multiplication
+import random
+import numpy as np
+M1 = np.random.randint(0,50,(5,10))
+M2 = np.random.randint(0,50,(10,5))
+print(M1)
+print(M2)
+
+def Matrix_multip():
+    rows1 = 5
+    columns1 = 10
+    rows2 = 10
+    columns2 = 5
+    
+    result = []
+    for i in range(rows1):
+        row = []
+        for j in range(columns2):
+            sum = 0
+            for k in range(columns1):
+                sum += M1[i][k] * M2[k][j]
+            row.append(sum)
+        result.append(row)
+    return result
+Matrix_multip()
